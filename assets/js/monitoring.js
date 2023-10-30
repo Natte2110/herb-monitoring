494478387a01f0a0ce97d408e7ecfea06b119270
Natte2110 <natteparsley@gmail.com>
2023-10-30 13:05:20 +0000

    feat: Add basic ArcGIS Web Map

---
+++ assets/js/monitoring.js
@@ -0,0 +1,19 @@
+require([
+    "esri/Map", 
+    "esri/views/MapView"
+], function( 
+    Map, 
+    MapView) {
+
+    const map = new Map({
+      basemap: "dark-gray-vector" // basemap styles service
+    });
+
+    const view = new MapView({
+      map: map,
+      center: [-4, 54], // Longitude, latitude
+      zoom: 5, // Zoom level
+      container: "monitoring-map" // Div element
+    });
+  
+  });
