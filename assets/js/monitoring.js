c854e3c3f7f8da73c15cf0209cf0c06835110bc4
Natte2110 <natteparsley@gmail.com>
2023-10-30 15:36:11 +0000

    feat: Add event listener which reorders the cards and refreshes map

--- assets/js/monitoring.js
+++ assets/js/monitoring.js
@@ -1,26 +1,43 @@
 require([
-    "esri/Map", 
-    "esri/views/MapView"
-], function( 
-    Map, 
-    MapView) {
-
-    const monitoringChoices = document.getElementById("monitoring-items");
-    let monitorChoice = "traffic";
-
-    const displayMap = (layer) => {
-      const map = new Map({
-        basemap: "dark-gray-vector" // basemap styles service
-      });
+  "esri/Map",
+  "esri/views/MapView"
+], function (
+  Map,
+  MapView) {
   
-      const view = new MapView({
-        map: map,
-        center: [-4, 54], // Longitude, latitude
-        zoom: 5, // Zoom level
-        container: "monitoring-map" // Div element
-      });
-      view.ui.add(monitoringChoices, "bottom-right")
+  const cardClasses = ["top-card","middle-card","bottom-card"]
+  const monitoringChoices = document.getElementById("monitoring-items");
+
+  const displayMap = (layer) => {
+    const map = new Map({
+      basemap: "dark-gray-vector" // basemap styles service
+    });
+
+    const view = new MapView({
+      map: map,
+      center: [-4, 54], // Longitude, latitude
+      zoom: 5, // Zoom level
+      container: "monitoring-map" // Div element
+    });
+    view.ui.add(monitoringChoices, "bottom-right")
+  }
+  monitoringChoices.addEventListener("click", function (event) {
+    let clickedID = event.target.parentElement
+    let orderItems = document.getElementsByClassName("monitoring-switch")
+    let newOrder = [clickedID.id];
+    for (let i = 0; i < orderItems.length; i++) {
+      
+      if (orderItems[i].id != newOrder[0]) {
+        newOrder.push(orderItems[i].id);
+      }
     }
-    
-    displayMap(monitorChoice)
+    newOrder.forEach(id => {
+      let card = document.getElementById(id)
+      card.classList.remove(card.classList[1])
+      card.classList.add(cardClasses[newOrder.indexOf(id)])
+    });
+    displayMap(clickedID.id);
   });
+  
+  displayMap("flood")
+});
