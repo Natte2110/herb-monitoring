cc307b6a7d162199b665474c85d814db0ceb5dc5
Natte2110 <natteparsley@gmail.com>
2023-10-30 22:25:31 +0000

    feat: Add function to display map for flood alerts

--- assets/js/monitoring.js
+++ assets/js/monitoring.js
@@ -1,9 +1,13 @@
 require([
   "esri/Map",
-  "esri/views/MapView"
+  "esri/views/MapView",
+  "esri/layers/FeatureLayer",
+  "esri/geometry/Point"
 ], function (
   Map,
-  MapView) {
+  MapView,
+  FeatureLayer,
+  Point) {
 
   const getData = (url) => {
     return fetch(url)
@@ -15,11 +19,10 @@ require([
       .catch(error => console.warn(error));
   }
 
-  getData("https://environment.data.gov.uk/flood-monitoring/id/floods").then(response => console.log(response));
   const cardClasses = ["top-card", "middle-card", "bottom-card"]
   const monitoringChoices = document.getElementById("monitoring-items");
 
-  const displayMap = (layer) => {
+  const displayMap = (id) => {
     /**
      * This function creates a map with the given monitoring layer and displays it in the "monitoring-map" div.
      */
@@ -34,6 +37,70 @@ require([
       container: "monitoring-map" // Div element
     });
     view.ui.add(monitoringChoices, "bottom-right")
+
+    const floodMap = () => {
+      /**
+       * This function is used to create the layers and required items for the flood monitoring map
+       * The reason two fetch API calls are used is due to the location and severity data being on different URLs
+       */
+      getData("https://environment.data.gov.uk/flood-monitoring/id/floodAreas")
+        .then(response => {
+          let itemsLocation = response.items;
+
+          getData("https://environment.data.gov.uk/flood-monitoring/id/floods")
+            .then(response => {
+              let itemsSeverity = response.items;
+              // console.log(itemsSeverity)
+              let merged = [];
+
+              for(let i=0; i<itemsSeverity.length; i++) {
+                if (itemsSeverity[i].severityLevel) {
+                  itemsLocation.forEach(item => {
+                    if (item.fwdCode === itemsSeverity[i].floodAreaID) {
+                      item.severityLevel = itemsSeverity[i].severityLevel
+                      item.severity = itemsSeverity[i].severity
+                      item.timeMessageChanged = itemsSeverity[i].timeMessageChanged
+                      merged.push(item);
+                    } 
+                  });
+                }
+              }
+              
+              let features = []; // Empty array that the points will be put into
+              let x = 1; //Used for incrementing object ID's
+
+              merged.forEach(item => {
+                features.push({
+                  geometry: new Point({ x: item.long, y: item.lat }),
+                  attributes: { 
+                    ObjectID: x, 
+                    County: item.County, 
+                    description: item.description,
+                    eaAreaName: item.eaAreaName,
+                    fwdCode: item.fwdCode, 
+                    severityLevel: item.severityLevel,
+                    severity: item.severity,
+                    polygon: item.polygon,
+                  }
+                })
+                x++ // Increment for the object ID's
+              })
+
+              let layer = new FeatureLayer({
+                source: features,  // autocast as a Collection of new Graphic()
+                objectIdField: "ObjectID"
+              });
+              map.layers.add(layer);
+            })
+
+
+        });
+    }
+    if (id === "flood") {
+      floodMap();
+    }
+
+
   }
   monitoringChoices.addEventListener("click", function (event) {
     /**
@@ -55,6 +122,7 @@ require([
       card.classList.remove(card.classList[1])
       card.classList.add(cardClasses[newOrder.indexOf(id)])
     });
+
     displayMap(clickedID.id);
   });
 
