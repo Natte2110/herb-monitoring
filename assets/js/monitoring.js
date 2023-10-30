c4305b3dc62d04f373bc62f16bc72047dbe15aae
Natte2110 <natteparsley@gmail.com>
2023-10-30 16:07:15 +0000

    feat: Add function to return data from a url

--- assets/js/monitoring.js
+++ assets/js/monitoring.js
@@ -4,8 +4,19 @@ require([
 ], function (
   Map,
   MapView) {
-  
-  const cardClasses = ["top-card","middle-card","bottom-card"]
+
+  const getData = (url) => {
+    return fetch(url)
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData);
+        return responseData;
+      })
+      .catch(error => console.warn(error));
+  }
+
+  getData("https://environment.data.gov.uk/flood-monitoring/id/floods").then(response => console.log(response));
+  const cardClasses = ["top-card", "middle-card", "bottom-card"]
   const monitoringChoices = document.getElementById("monitoring-items");
 
   const displayMap = (layer) => {
@@ -35,7 +46,6 @@ require([
     let orderItems = document.getElementsByClassName("monitoring-switch")
     let newOrder = [clickedID.id];
     for (let i = 0; i < orderItems.length; i++) {
-      
       if (orderItems[i].id != newOrder[0]) {
         newOrder.push(orderItems[i].id);
       }
