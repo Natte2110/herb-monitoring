1076583862ebb6aa69ac4092e2669147f9378d44
Natte2110 <natteparsley@gmail.com>
2023-11-01 20:30:51 +0000

    feat: Add test for dynamic scrolling values

--- assets/js/tests/style.test.js
+++ assets/js/tests/style.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const {navColourChange} = require("../style")
+const { navColourChange } = require("../style")
 
 beforeEach(() => {
     const fs = require('fs');
@@ -25,12 +25,27 @@ describe("Dynamic Style Effects", () => {
         });
         test("Expect navbar to change to an opacity of 2/3 the max value", () => {
             navColourChange(200);
-            let expectedOpacity = 200/300
+            let expectedOpacity = 200 / 300
             expect(document.getElementById("site-nav").style.backgroundColor).toEqual("rgba(40, 45, 50, " + expectedOpacity + ")");
         });
         test("Expect navbar opacity to still be 1 over the max scroll amount", () => {
             navColourChange(400);
             expect(document.getElementById("site-nav").style.backgroundColor).toEqual("rgb(40, 45, 50)");
+        });
+        test("Expect navbar opacity change dynamically when window is scrolled", () => {
+            // event listener for when the window is scrolled
+            window.addEventListener('scroll', () => {
+                navColourChange(document.documentElement.scrollTop);
+            });
+
+            // Send a scroll event to the mock dom
+            const scrollEvent = new Event('scroll');
+            window.dispatchEvent(scrollEvent);
+
+            // Get the expected opacity of the new background
+            let expectedOpacity = document.documentElement.scrollTop / 300
+            
+            expect(document.getElementById("site-nav").style.backgroundColor).toEqual("rgba(40, 45, 50, " + expectedOpacity + ")");
         });
     });
 });
