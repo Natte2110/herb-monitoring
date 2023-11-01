66064ce067b6a24994bf9e9ee1c43ff32ef59179
Natte2110 <natteparsley@gmail.com>
2023-11-01 20:13:46 +0000

    feat: Change background colour when function called

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,5 +1,9 @@
-const navColourChange = (scrollValue) => {
+/**
+ * @jest-environment jsdom
+ */
 
+const navColourChange = (scrollValue) => {
+    document.getElementById("site-nav").style.backgroundColor = "rgba(40, 45, 50, 1)"
 }
 
-module.exports = navColourChange;
+module.exports = {navColourChange};
