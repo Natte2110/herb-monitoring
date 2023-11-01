f65d8095b06203df65a6502ceb4eef9b197935dc
Natte2110 <natteparsley@gmail.com>
2023-11-01 19:56:55 +0000

    feat: Add test for background color change

--- assets/js/tests/style.test.js
+++ assets/js/tests/style.test.js
@@ -14,9 +14,13 @@ beforeEach(() => {
 
 describe("Dynamic Style Effects", () => {
     describe("Navbar Colour Changing", () => {
-        test("Expect Navbar Colour to Change", () => {
+        test("Expect Navbar Colour to be transparent", () => {
             document.documentElement.scrollTop = 0;
             expect(document.getElementById("site-nav").style.backgroundColor).toEqual("");
+        });
+        test("Expect navbar to change to an opaque background", () => {
+            document.documentElement.scrollTop = 300;
+            expect(document.getElementById("site-nav").style.backgroundColor).toEqual("rgb(40, 45, 50, 1)");
         })
-    })
-})
+    });
+});
