258bdd4907e02cc01d45407bcaf7990fb4a42a72
Natte2110 <natteparsley@gmail.com>
2023-11-01 20:31:31 +0000

    feat: Add event listener for window scroll

--- assets/js/style.js
+++ assets/js/style.js
@@ -2,6 +2,10 @@
  * @jest-environment jsdom
  */
 
+window.addEventListener('scroll', () => {
+    navColourChange(document.documentElement.scrollTop);
+});
+
 const navColourChange = (scrollValue) => {
     let opacity = scrollValue / 300;
     document.getElementById("site-nav").style.backgroundColor = "rgba(40, 45, 50, " + opacity + ")"
