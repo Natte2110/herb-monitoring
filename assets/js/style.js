ceb2ed7d29fc553b576cc7664569842905f5dd9c
Natte2110 <natteparsley@gmail.com>
2023-11-03 08:51:41 +0000

    fix: Validate JS Code

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,7 +1,3 @@
-/**
- * @jest-environment jsdom
- */
-
 window.addEventListener('scroll', () => {
     navColourChange(document.documentElement.scrollTop);
     backgroundBlurChange(document.documentElement.scrollTop);
@@ -9,13 +5,11 @@ window.addEventListener('scroll', () => {
 
 const navColourChange = (scrollValue) => {
     let opacity = scrollValue / 300;
-    document.getElementById("site-nav").style.backgroundColor = "rgba(40, 45, 50, " + opacity + ")"
-}
+    document.getElementById("site-nav").style.backgroundColor = "rgba(40, 45, 50, " + opacity + ")";
+};
 
 const backgroundBlurChange = (scrollValue) => {
     let blur = 10 * (scrollValue / 600);
-    blur >= 10 ? blur = 10 : blur = blur;
+    blur = Math.min(blur, 10);
     document.getElementsByClassName("landing-image")[0].style.filter = "blur(" + blur + "px)";
-}
-
-// module.exports = {navColourChange, backgroundBlurChange};
+};
